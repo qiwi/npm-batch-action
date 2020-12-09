e0e570f57c9f6aa3b8e3c797959430aff6d5277f
Choro Abdymanapov <c.abdymanapov@qiwi.com>
2020-12-09 11:35:41 +0300

    fix(cli-pipe): add error handling

--- packages/cli-pipe/src/main/ts/index.ts
+++ packages/cli-pipe/src/main/ts/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { run } from '@qiwi/npm-batch-cli-api'
+
 const stdin = process.openStdin()
 
 let configRaw = ''
@@ -8,7 +9,11 @@ stdin.on('data', (chunk) => {
   configRaw += chunk
 })
 
-stdin.on('end', async () => {
-  await run(configRaw)
-  process.exit()
-})
+stdin.on('end', () =>
+  run(configRaw)
+    .then(() => process.exit())
+    .catch(e => {
+      console.error(e)
+      process.exit(1)
+    })
+)
